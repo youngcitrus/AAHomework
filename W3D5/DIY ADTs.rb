58d1564287dd29ee636274be6f537538a60016f0
youngcitrus <mrjustinnguyen@gmail.com>
2019-09-06 00:47:47 -0700

    Created Stack class with basic instance methods

---
+++ W3D5/DIY ADTs.rb
@@ -0,0 +1,22 @@
+class Stack
+    def initialize(array=[])
+      @stack = array
+      # create ivar to store stack here!
+    end
+
+    def push(el)
+      # adds an element to the stack
+      @stack << el
+    end
+
+    def pop
+      # removes one element from the stack
+      @stack.pop
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+      @stack.last
+    end
+  
+end
